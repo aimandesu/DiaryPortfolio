6ee83aa47c336cea4e00de7dab0bde6eb240377e
aimandesu <aimanafiqbinesam@gmail.com>
2025-12-15 01:45:19 +0800

    add token handler

---
+++ DiaryPortfolio.Application/IRepository/ITokenRepository/ITokenRepository.cs
@@ -0,0 +1,14 @@
+﻿using DiaryPortfolio.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryPortfolio.Application.IRepository.ITokenRepository
+{
+    public interface ITokenRepository
+    {
+        TokenModel GenerateToken(string Email, Guid UserId);
+    }
+}
---
+++ DiaryPortfolio.Domain/Entities/TokenModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryPortfolio.Domain.Entities
+{
+    public class TokenModel
+    {
+        public String JWTAccessToken { get; set; } = string.Empty;
+        public String RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow;
+    }
+}
